791c0829822fec2363858b9a38a9b1b4743184ef
Binay Paul <binay.paul.1993@gmail.com>
2026-03-16 14:49:31 +0530

    Initial setup

---
+++ basedomains/src/test/java/com/binay/basedomains/BasedomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binay.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasedomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ stockservice/src/test/java/com/binay/stockservice/StockserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binay.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
